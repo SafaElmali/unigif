e4bdf2d03017a159b97a753f296f959344ecbcae
SafaElmali <tsafaelmali@gmail.com>
2019-11-25 22:27:38 +0300

    added search area

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Layout, Typography, Row, Col, Input, Button } from 'antd';
+const { Footer, Sider, Content } = Layout;
+const { Text } = Typography;
+const { Search } = Input;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+    render() {
+        return (
+            <Layout>
+                <Content>
+                    <Row className="header">
+                        <Col xs={24} className="header-props">
+                            <Row className="header-row-props text-center-row">
+                                <Col xs={24}>
+                                    <Text className="header-brand-text text-color">Unigif</Text>
+                                </Col>
+                                <Col xs={24}>
+                                    <Text strong className="header-text text-color">Make universal search to find funny GIFs, reaction GIFs, unique GIFs and more.</Text>
+                                </Col>
+                            </Row>
+                            <Row type="flex" justify="center" className="search-area-row">
+                                <Col xs={24} sm={12} md={12} lg={6}>
+                                    <Search
+                                        placeholder="Search all gifs"
+                                        onSearch={value => console.log(value)}
+                                        className="search-props"
+                                    />
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Content>
+            </Layout>
+        )
+    }
 }
-
-export default App;
