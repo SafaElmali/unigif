b201dfbd497cd41110ba30238eda7a426711b041
SafaElmali <tsafaelmali@gmail.com>
2019-11-29 21:31:27 +0300

    function to class component

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Typography, Col } from 'antd';
 
 const { Title, Text } = Typography;
 
-function GifCard(props) {
-    return (
-        <Col>
-            <div className="gif-card">
-                <div className="gif-header">
-                    <a href={props.web_url} target="_blank" rel="noopener noreferrer"><img src={props.original_url} alt={props.title === '' ? 'No title' : props.title} /></a>
+export default class GifCard extends Component {
+    render() {
+        let { web_url, original_url, title } = this.props;
+        return (
+            <Col>
+                <div className="gif-card">
+                    <div className="gif-header">
+                        <a href={web_url} target="_blank" rel="noopener noreferrer"><img src={original_url} alt={title === '' ? 'No title' : title} /></a>
+                    </div>
+                    <div className="gif-body">
+                        <Title level={4}>{title === '' ? 'No title' : title}</Title>
+                        <Text type="secondary"><a href={web_url} target="_blank" rel="noopener noreferrer">Go to source</a></Text>
+                    </div>
                 </div>
-                <div className="gif-body">
-                    <Title level={4}>{props.title === '' ? 'No title' : props.title}</Title>
-                    <Text type="secondary"><a href={props.web_url} target="_blank" rel="noopener noreferrer">Go to source</a></Text>
-                </div>
-            </div>
-        </Col>
-    )
+            </Col>
+        )
+    }
 }
 
-export default GifCard;
